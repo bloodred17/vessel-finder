1b7f79b423619f46fd7d3892ecfd1d0a3b9e03cc
Ankur Dutta <ankur@tatxtion.tech>
2024-02-28 00:07:37 +0530

    Removing db

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import {VesselFinderScraper} from "./scrape.ts";
 import {Command, WorkerMessage} from "./command.ts";
-import {mongoose} from "@typegoose/typegoose";
-import {VesselSchema} from "./vessel.schema.ts";
 
-console.log('Connecting to db...');
-const url = 'mongodb://127.0.0.1:27017/'
-await mongoose.connect(url, {
-  dbName: 'test',
-});
 
 const workerURL = new URL("worker.ts", import.meta.url).href;
 const worker = new Worker(workerURL);
